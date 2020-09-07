8534c8f215ebf96ab8331452751afaca75200e96
cjwind <cwentsai@gmail.com>
2020-09-07 17:59:47 +0800

    Init

---
+++ src/Tennis.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+class Tennis
+{
+    public function getScore(): string
+    {
+
+    }
+}
---
+++ tests/TennisTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Test;
+
+use PHPUnit\Framework\TestCase;
+
+class TennisTest extends TestCase
+{
+
+}
